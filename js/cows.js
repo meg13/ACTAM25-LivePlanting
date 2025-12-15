720c0cc5f38f883810afe0823da7bb1a39fc329f
Margherita Raponi <raponi.margherita.rm@gmail.com>
2025-12-15 10:42:29 +0100

    Added different colors for cows

--- js/cows.js
+++ js/cows.js
@@ -1,6 +1,19 @@
 class SimpleCow {
+    // Palette di colori condivisa tra tutte le mucche
+    static colorPalettes = [
+        { body: '#A6561B', details: '#854516' },
+        { body: '#4B3627', details: '#5D4A3D' },
+        { body: '#F5D09F', details: '#D3A463' },
+        { body: '#A0826D', details: '#8B6F5E' }
+    ];
+    static colorIndex = 0;
+
     constructor() {
         this.element = null;
+
+        // Assegna i colori a giro
+        this.colors = SimpleCow.colorPalettes[SimpleCow.colorIndex];
+        SimpleCow.colorIndex = (SimpleCow.colorIndex + 1) % SimpleCow.colorPalettes.length;
 
         // Calcola l'area valida evitando il nav
         const nav = document.querySelector('nav');
@@ -18,11 +31,23 @@ class SimpleCow {
 
         this.create();
     }
-    
+
     create() {
         this.element = document.createElement('div');
         this.element.className = 'walking-cow';
-        this.element.innerHTML = `<img src="../images/cow.svg" alt="cow">`;
+        this.element.innerHTML = `
+            <svg width="80" height="64" viewBox="0 0 80 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 22C0 18.6863 2.68629 16 6 16H58C61.3137 16 64 18.6863 64 22V58C64 61.3137 61.3137 64 58 64H6C2.68629 64 0 61.3137 0 58V22Z" fill="${this.colors.body}"/>
+                <path d="M32 6C32 2.68629 34.6863 0 38 0H66C69.3137 0 72 2.68629 72 6V34C72 37.3137 69.3137 40 66 40H38C34.6863 40 32 37.3137 32 34V6Z" fill="${this.colors.body}"/>
+                <rect width="16" height="16" transform="translate(64 16)" fill="${this.colors.details}"/>
+                <path d="M68 20C68 17.7909 69.7909 16 72 16C74.2091 16 76 17.7909 76 20C76 22.2091 74.2091 24 72 24C69.7909 24 68 22.2091 68 20Z" fill="white"/>
+                <path d="M56 12C56 9.79086 57.7909 8 60 8C62.2091 8 64 9.79086 64 12C64 14.2091 62.2091 16 60 16C57.7909 16 56 14.2091 56 12Z" fill="black"/>
+                <path d="M64 10C64 8.89543 64.8954 8 66 8C67.1046 8 68 8.89543 68 10C68 11.1046 67.1046 12 66 12C64.8954 12 64 11.1046 64 10Z" fill="white"/>
+                <rect width="12" height="12" transform="translate(36)" fill="${this.colors.details}"/>
+                <rect width="12" height="12" transform="translate(8 52)" fill="${this.colors.details}"/>
+                <rect width="12" height="12" transform="translate(28 52)" fill="${this.colors.details}"/>
+            </svg>
+        `;
         this.element.style.left = this.x + 'px';
         this.element.style.top = this.y + 'px';
         this.element.style.transform = `scaleX(${this.direction})`;
